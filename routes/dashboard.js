52469f5aeef0ad1fcb71589ba9115495f57dac8c
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-01-10 16:20:20 +0100

    Done V45

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,13 +2,11 @@ var express = require("express");
 var router = express.Router();
 const artistModel = require("../models/Artist.model");
 
-router.get("/", (req, res, next) => {
+router.get("/artists", (req, res, next) => {
   artistModel
     .find()
-    .then((dbResponse) => {
-      res.render("dashboard/artists.hbs", {
-        artists: dbResponse,
-      });
+    .then((artists) => {
+      res.render("dashboard/artists.hbs", { artists });
     })
     .catch((e) => console.error(e));
 });
@@ -17,33 +15,7 @@ router.get("/createArtist", function (req, res, next) {
   res.render("dashboard/createArtist.hbs");
 });
 
-router.post("/createArtist", (res, req, next) => {
-  artistModel
-    .create(req.body)
-    .then(res.redirect("/dashboard"))
-    .catch((err) => next(err));
-});
-
-router.get("/update/:id", function (req, res, next) {
-  id = req.params.id;
-  artistModel
-    .findById(id)
-    .then((dbResponse) => {
-      console.log(dbResponse);
-      res.render("artistUpdate.hbs", { dbResponse });
-    })
-    .catch((error) => next(error));
-});
-
-router.get("/delete/:id", function (req, res, next) {
-  id = req.params.id;
-  artistModel
-    .findByIdAndDelete(id)
-    .then(() => res.redirect("/dashboard/artists"))
-    .catch((error) => next(error));
-});
-
-// router.post("/", async function (req, res, next) {
+// router.post("/createArtist", async function (req, res, next) {
 //   try {
 //     await artistModel.create(req.body);
 //     console.log("Ok post");
@@ -53,13 +25,40 @@ router.get("/delete/:id", function (req, res, next) {
 //   }
 // });
 
-router.post("/update/:id", async function (req, res, next) {
+router.post("/createArtist", (req, res, next) => {
+  console.log(req.body);
+  artistModel
+    .create(req.body)
+    .then(res.redirect("/dashboard/artists"))
+    .catch((err) => next(err));
+});
+
+router.get("/artists/delete/:id", function (req, res, next) {
+  const id = req.params.id;
+  artistModel
+    .findByIdAndDelete(id)
+    .then(res.redirect("/dashboard/artists"))
+    .catch((error) => next(error));
+});
+
+router.get("/artists/update/:id", function (req, res, next) {
+  const id = req.params.id;
+  artistModel
+    .findById(id)
+    .then((foundArtist) => {
+      res.render("dashboard/artistUpdate.hbs", { artist: foundArtist });
+    })
+    .catch((error) => next(error));
+});
+
+router.post("/artists/update/:id", async function (req, res, next) {
   try {
-    id = req.params.id;
+    const id = req.params.id;
+    console.log("This is the id >>>>>>>", id);
+    console.log("This is the req.body >>>>>>>", req.body);
     await artistModel.findByIdAndUpdate(id, req.body, {
       new: true,
     });
-    console.log("Ok update");
     res.redirect("/dashboard/artists");
   } catch (err) {
     next(err);
